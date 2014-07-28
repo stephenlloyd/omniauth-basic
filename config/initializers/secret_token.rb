23328bc9273bd98dbecff8b0dc4a20efc0bbb82c
Stephen Lloyd <stephen@Stephens-MacBook-Pro.local>
2014-07-28 15:58:49 +0100

    omniauth basic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chickenwings::Application.config.secret_key_base = '441c448f283c5d0b1c95aefd300c61e383be0adb23b7855bd58c57006b693c6bed9cb4dd051c44bc5f1a27f70d7ca1299615fc0501c6746f7aeea51fc8f621da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chickenwings::Application.config.session_store :cookie_store, key: '_chickenwings_session'
